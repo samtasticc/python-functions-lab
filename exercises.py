7f987f2cf622348f72f4f42b133a92d1ab1194ba
samtasticc <samantha.henesy@gmail.com>
2025-01-15 20:58:35 -0500

    exercise 2 pass

--- exercises.py
+++ exercises.py
@@ -11,7 +11,30 @@
 #
 # Define your function and call it below.
 
-def calculate_area_triangle(b, h):
-    return ((b * h)/2)
+# def calculate_area_triangle(b, h):
+#     return ((b * h)/2)
 
-print('Exercise 1:', calculate_area_triangle(10, 5), calculate_area_triangle(7, 3))
+# print('Exercise 1:', calculate_area_triangle(10, 5), calculate_area_triangle(7, 3))
+
+# ------------ EXERCISE 2
+
+# Exercise 2: Calculate Simple Interest
+#
+# Write a function named `simple_interest` that takes principal, rate of interest (as a percentage), and time (years).
+# Calculate and return the simple interest using the formula (principal * rate * time) / 100.
+#
+# Examples:
+# simple_interest(1000, 5, 2) should return 100.
+# simple_interest(1500, 3.5, 5) should return 262.5.
+#
+# Define your function and call it to see the result.
+
+def simple_interest(principal, rate, term):
+    return ((principal * rate * term)/100)
+
+print('Exercise 2:', simple_interest(1000, 5, 2), simple_interest(1500, 3.5, 5))
+
+# ------------ EXERCISE 3
+
+
+
